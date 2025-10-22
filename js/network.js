55d32c02082cc059488af09b9cf0ed40541748f5
Hodaya Glikstein <hodayaglikstein@hilma.tech>
2025-10-22 17:04:42 +0300

    notthing

--- js/network.js
+++ js/network.js
@@ -1,13 +1,10 @@
-function network(request){
-    setTimeout(function(request){
-      const random = Math.random();
-    if(random<0.1){
-        return console.log("error");
-    }else{
-        return request;
-    }  
-    }, 3000)
-    
-    
-    
+function network(request) {
+  setTimeout(function (request) {
+    const random = Math.random();
+    if (random < 0.1) {
+      return console.log("error");
+    } else {
+      return request;
+    }
+  }, 3000);
 }

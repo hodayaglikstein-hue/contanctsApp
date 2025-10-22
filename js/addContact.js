933c04264767dc53591ff408cea4473042a9d00f
Hodaya Glikstein <hodayaglikstein@hilma.tech>
2025-10-22 15:56:40 +0300

    fajax

--- js/addContact.js
+++ js/addContact.js
@@ -7,14 +7,18 @@ function addContact() {
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phonenumber").value;
 
-    const contact = {
-      firstname: firstname,
-      lastname: lastname,
-      email: email,
-      phone: phone,
-    };
+    if (firstname === "" || lastname === "" || email === "" || phone === "") {
+      alert("please fill everything");
+    } else {
+      const contact = {
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        phone: phone,
+      };
 
-    newContact(contact);
-    navigate("app");
+      newContact(contact);
+      navigate("app");
+    }
   }
 }
